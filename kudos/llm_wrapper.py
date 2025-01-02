b3865610e2db0f7109d727f98523e297f12f3991
James Stevenson <28961606+user1342@users.noreply.github.com>
2025-01-02 22:57:10 +0000

    Update llm_wrapper.py

--- kudos/llm_wrapper.py
+++ kudos/llm_wrapper.py
@@ -6,7 +6,12 @@ import random
 
 models = ["unsloth/mistral-7b-instruct-v0.3-bnb-4bit","unsloth/mistral-7b-instruct-v0.3","unsloth/Qwen2-VL-7B-Instruct-unsloth-bnb-4bit","unsloth/SmolLM-1.7B-Instruct","unsloth/Llama-3.1-Storm-8B"]
 
-def ask_question(question, schema, max_new_tokens=500, llm_name=random.choice(models):
+def ask_question(question, schema, max_new_tokens=500, llm_name=None:
+
+    if llm_name == None:
+        llm_name = random.choice(models)
+        print(f"Choosing model {llm_name}")
+    
     llm = EasyLLM(llm_name)
     #schema_model = llm.create_pydantic_model_from_schema(schema)
     response = llm.ask_question_with_schema(prompt=question, json_schema=schema, max_new_tokens=max_new_tokens)
