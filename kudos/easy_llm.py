cef0a360d992fb5a0633c984b6afe353f0882b80
James Stevenson <28961606+user1342@users.noreply.github.com>
2025-01-02 21:24:21 +0000

    Update easy_llm.py

--- kudos/easy_llm.py
+++ kudos/easy_llm.py
@@ -39,7 +39,7 @@ class EasyLLM:
         print("Model loaded successfully.")
 
     def ask_question(self, prompt: str, max_new_tokens: int = 300) -> str:
-        temperature = random.uniform(0.1, 2.0)
+        temperature = random.uniform(1.0, 1.3)
         
         inputs = self._tokenizer(prompt, return_tensors="pt")
         input_ids = inputs["input_ids"].to(self._device)
@@ -69,7 +69,7 @@ class EasyLLM:
         return DynamicModel
 
     def ask_question_with_schema(self, prompt: str, json_schema: dict, max_new_tokens: int = 128) -> dict:
-        temperature = random.uniform(0.7, 2.0)
+        temperature = random.uniform(1.0, 1.3)
         jsonformer = Jsonformer(
             self._model,
             self._tokenizer,
