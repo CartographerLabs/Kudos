9947efa4f292c97d6992a9c0dc7bb8c7c8619dfc
James Stevenson <28961606+user1342@users.noreply.github.com>
2025-01-02 16:41:51 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 
 from setuptools import setup, find_packages
 
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [
+        line.strip()
+        for line in fh
+        if line.strip() and not line.startswith("#")
+    ]
+
 setuptools.setup(
     name="kudos",
     version="0.1.0",
     packages=setuptools.find_packages(),
-    install_requires=[
-        "networkx",
-        "filelock",
-        "torch",
-        "transformers",
-        "jsonformer",
-        "pydantic"
-    ],
+    install_requires=requirements,
     entry_points={
         "console_scripts": [
             "wargame-run=game.run_simulation:main"
