4f32a438b0d34d4c772845f39ec6c6ce8545d449
James Stevenson <28961606+user1342@users.noreply.github.com>
2025-01-03 10:32:00 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="kudos",
-    version="0.3.15",
+    version="0.3.16",
     packages=setuptools.find_packages(),
     install_requires=requirements,
     entry_points={
